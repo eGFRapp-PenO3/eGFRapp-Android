f1e3a25439ed737006221f912374b9f192332401
stormeuh <elias.storme@gmail.com>
2016-12-22 01:23:47 +0100

    Starting creation of custom cursoradapter for history

---
+++ app/src/main/java/be/kulak/peo/egfr/HistoryLoader.java
@@ -0,0 +1,14 @@
+package be.kulak.peo.egfr;
+
+import android.app.ListActivity;
+import android.app.LoaderManager;
+import android.database.Cursor;
+
+/**
+ * Created by elias on 22/12/2016.
+ */
+
+public class HistoryLoader extends ListActivity
+        implements LoaderManager.LoaderCallbacks<Cursor>{
+
+}
